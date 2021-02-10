f0ae014a9e6a19c1749bb85b2e53ac2eb126acda
Ulas Gurukumar <ullas.g.123@gmail.com>
2021-02-09 21:16:37 -0600

    fixed code, creates a LL with random pointer

--- LeetCode/Copy LL with Random Ptr/LLWithRandPtr.py
+++ LeetCode/Copy LL with Random Ptr/LLWithRandPtr.py
@@ -1,3 +1,7 @@
+import array
+import random
+
+
 class Node:
     def __init__(self, data=None, next=None, randomPointer=None):
         self.data = data
@@ -8,20 +12,22 @@ class Node:
         return hash((self.data, self.next, self.randomPointer))
 
     def __eq__(self, other):
-        print(self.data)
-        isEqual = self.data == other.data
-        # print(type(other))
-        if (self.next != None and other.next != None):
-            isEqual = self.next.data == other.next.data
-        elif (self.next == None and other.next != None) or (self.next != None and other.next == None):
-            return False
+        if isinstance(other, Node):
+            isEqual = self.data == other.data
 
-        if (self.randomPointer != None and other.randomPointer != None):
-            isEqual = self.randomPointer.data == other.randomPointer.data
-        elif (self.randomPointer == None and other.randomPointer != None) or (self.randomPointer != None and other.randomPointer == None):
-            return False
+            if (self.next != None and other.next != None):
+                isEqual = self.next.data == other.next.data
+            elif (self.next == None and other.next != None) or (self.next != None and other.next == None):
+                return False
 
-        return isEqual
+            if (self.randomPointer != None and other.randomPointer != None):
+                isEqual = self.randomPointer.data == other.randomPointer.data
+            elif (self.randomPointer == None and other.randomPointer != None) or (self.randomPointer != None and other.randomPointer == None):
+                return False
+
+            return isEqual
+
+        return False
 
     def __ne__(self, other):
         return not(self == other)
@@ -32,25 +38,25 @@ class LinkedList:
         self.head = None
 
     def listprint(self):
-        printval = self.head
-        while printval is not None:
-            print(printval.data)
-            # print(printval.randomPointer)
-            printval = printval.next
+        currNode = self.head
+        while currNode is not None:
+            if (currNode.randomPointer == None):
+                print("data:", currNode.data, "Random Pointer NULL")
+            else:
+                print("data:", currNode.data, "Random Pointer:",
+                      currNode.randomPointer.data)
+            currNode = currNode.next
 
+
+nodeList = [Node(i) for i in range(10)]
+
+for i in range(9):
+    nodeList[i].next = nodeList[i + 1]
+    randIndex = random.randint(0, 19)
+    if randIndex < 10:
+        nodeList[i].randomPointer = nodeList[randIndex]
 
 list = LinkedList()
-list.head = Node(0)
-
-node1 = Node(data=1)
-copyNode1 = Node(data=1)
-node2 = Node(data=2)
-node1.next = node2
-list.head.next = node1
+list.head = nodeList[0]
 
 list.listprint()
-
-# print(hash(node1) == hash(node2))
-# print(hash(node1) == hash(copyNode1))
-print(node1 == node2)
-print(node1 == copyNode1)
