b06a4af3d07dc503f2b23d560ad8ba7abff7e668
Ulas Gurukumar <ullas.g.123@gmail.com>
2021-02-20 20:43:01 -0600

    init commit of heap stuff

---
+++ Others/Heap/MinHeap.js
@@ -0,0 +1,14 @@
+class MinHeap {
+  constructor() {
+    this.heap = []
+  }
+
+  insert(val) {
+    this.heap.unshift(val)
+  }
+
+  balance() {
+    let i = 0
+    while (i < this.heap.length) {}
+  }
+}
